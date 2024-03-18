17f3ae111fd555f79528a28e90fd96edb423ba58
Kimi310 <tymon.mikula@gmail.com>
2024-03-18 12:05:40 +0100

    event coordinator view finished

---
+++ src/GUI/Controller/EventCoordinatorViewController.java
@@ -0,0 +1,14 @@
+package GUI.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class EventCoordinatorViewController {
+    @FXML
+    private void addEvent(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void deleteEvent(ActionEvent actionEvent) {
+    }
+}
