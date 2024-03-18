969ac1815e7c1861ed984427a7016e900c0c51e9
EchoFD54 <144889612+EchoFD54@users.noreply.github.com>
2024-03-18 16:52:32 +0100

    Revert "textFields refactoring"
    This reverts commit 535e32fb32125c7e9462a7ae5f65c2cab072e264.

--- src/GUI/Controller/CreateEventViewController.java
+++ src/GUI/Controller/CreateEventViewController.java
@@ -5,33 +5,29 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class CreateEventViewController {
-    public TextField eventNameTF;
+    public TextField eventNameField;
     private EventCoordinatorViewController eventCoordinatorController;
     public void confirmCreateEventBtn(ActionEvent actionEvent) {
-        String name = eventNameTF.getText();
+        String name = eventNameField.getText();
 
         //update/add event properties on the EventCoordinator class
         eventCoordinatorController.updateEventProperties(name);
 
         //close the CreateEvent Window
-        ((Stage) eventNameTF.getScene().getWindow()).close();
+        ((Stage) eventNameField.getScene().getWindow()).close();
     }
 
     public void cancelCreateEventBtn(ActionEvent actionEvent) {
-        ((Stage) eventNameTF.getScene().getWindow()).close();
     }
+
+    public void dateNewEventBtn(ActionEvent actionEvent) {
+    }
+
+
+
 
 
     public void setEventCoordinatorController(EventCoordinatorViewController eventCoordinatorController) {
         this.eventCoordinatorController = eventCoordinatorController;
     }
-
-    public void startingDateEventBtn(ActionEvent actionEvent) {
-    }
-
-
-    public void endingDateEventBtn(ActionEvent actionEvent) {
-    }
-
-
 }
