6333581945f450eba899e133bf1f04d4c1ce5eec
EchoFD54 <144889612+EchoFD54@users.noreply.github.com>
2024-04-08 14:52:03 +0200

    printing
    tickets can be printed, have to fix the layout tho because the pdf files are weird and dont show the ticket properly

--- src/GUI/Controller/TicketNController.java
+++ src/GUI/Controller/TicketNController.java
@@ -1,12 +1,18 @@
 package GUI.Controller;
 
 import BE.Ticket;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.print.PrinterJob;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+
 
 public class TicketNController {
     public Label ticketNamelabel, ticketEmailLabel, serialNumberLabel, ticketPriceLabel, ticketLocationLabel, ticketTimeLabel;
+    public AnchorPane ticketPane;
 
     private Stage stage;
 
@@ -34,4 +40,16 @@ public class TicketNController {
         this.stage = stage;
     }
 
+
+
+
+    public void printTicket(ActionEvent actionEvent) {
+        PrinterJob printerJob = PrinterJob.createPrinterJob();
+        if (printerJob != null && printerJob.showPrintDialog(stage)) {
+            boolean success = printerJob.printPage(ticketPane);
+            if (success) {
+                printerJob.endJob();
+            }
+        }
+    }
 }
